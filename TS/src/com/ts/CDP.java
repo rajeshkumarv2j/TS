b06beb321f62ebaaf8bd5ab5ad25e60a16d03c8c
Rajeshkumar <rajeshkumar.v2j@gmail.com>
2016-04-04 03:00:50 +0530

    Initial Commit

---
+++ TS/src/com/ts/CDP.java
@@ -0,0 +1,7 @@
+package com.ts;
+
+public class CDP {
+	public static void main(String[] args) {
+		System.out.println("Working on CDP");
+	}
+}
---
+++ TS/src/com/ts/UW.java
@@ -0,0 +1,7 @@
+package com.ts;
+
+public class UW {
+	public static void main(String[] args) {
+		System.out.println("Working on UW");
+	}
+}
